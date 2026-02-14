4a26aa4670426f78f7342c0d660c248ceae811de
Lucas Tumeleiro <lucasftumeleiro@gmail.com>
2026-02-14 02:47:57 -0300

    refactor: frontend - Ajuste layout e limpeza dos testes na Home

--- web/src/Layout/index.tsx
+++ web/src/Layout/index.tsx
@@ -3,10 +3,10 @@ import { Sidebar } from "./components/Sidebar";
 
 function Layout() {
   return (
-    <div className="flex h-screen w-screen bg-primary-lighter">
+    <div className="flex h-screen w-screen bg-primary-lighter overflow-hidden">
       <Sidebar />
 
-      <main className="flex-1 ">
+      <main className="flex-1 overflow-y-auto m-8">
         <Outlet />
       </main>
     </div>
